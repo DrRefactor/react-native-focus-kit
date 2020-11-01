1c08c6619e668b9e27c8bd0d655d95329ea9a302
drrefactor <drrefactor@gmail.com>
2020-11-01 20:51:41 +0100

    Export arrow emitter types

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
+import {ArrowKeyEventEmitter} from './hooks/useKeyDownListener';
+// TODO: remove to type-only re-export after upgrading typescript to 3.8<
+export type ArrowKeyEventsEmitter = ArrowKeyEventEmitter
+
 export {default as Focusable} from './spatial/Focusable';
 export {default as FocusableLayer, useFocusableLayer} from './spatial/FocusableLayer';
 export {default as MainFocusController} from './spatial/MainFocusController';
+export {ArrowKeyEvent, ArrowKey} from './hooks/useKeyDownListener';
+
 
 export {useFocusNavigator, useMeasure} from './spatial/SpatialHooks';
